2df7eadd54af0655e843b1816e0bd66754f4ea38
georgeclaudiuIL <ibanescudodo@yahoo.com>
2025-04-28 23:06:27 +0300

    Adaugare eliminare_produs in ProductManager pe ramura add-product-removal

--- product_manager.py
+++ product_manager.py
@@ -21,6 +21,15 @@ class ProductManager:
             total += product['price'] * product['quantity']
         print(f"Valoarea totala a produselor este {total}.")
 
+    def remove_product(self, product_name):
+        for product in self.products:
+            if product["name"] == product_name:
+                self.products.remove(product)
+                print(f"{product_name} a fost eliminat.")
+                return
+        print(f"Produsul {product_name} nu a fost gasit.")
+
+
 # lista_produse = ProductManager()
 # print()
 # lista_produse.add_products("Laptop", 2500, 3)
