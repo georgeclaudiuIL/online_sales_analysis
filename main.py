91faa27dd7db75002774a3f2b9ddb79c372da3dd
georgeclaudiuIL <ibanescudodo@yahoo.com>
2025-04-28 23:51:44 +0300

    Commit main.py din ramura main

--- main.py
+++ main.py
@@ -3,11 +3,7 @@ from product_manager import ProductManager
 
 product_manager = ProductManager()
 
-product_manager.add_products("Laptop", 2500, 3)
-product_manager.add_products("Monitor", 700, 4)
-product_manager.add_products("Mouse", 100, 20)
-print()
-product_manager.show_products()
-print()
-product_manager.total_value()
+product_manager.add_products("Sursa", 2500, 3)
+product_manager.add_products("Placa de baza", 700, 4)
+product_manager.add_products("Placa video", 100, 20)
 print()
